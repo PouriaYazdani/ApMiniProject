ecf8aa197a916ed1f0dc21e0824c5587d68d1dfc
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-23 12:31:30 +0430

    added constructor and color variable and it's getter

--- game/properties/Cinema.java
+++ game/properties/Cinema.java
@@ -2,4 +2,13 @@ package game.properties;
 
 public class Cinema extends BuyableProperties{
     public final static int[] atFields = {4,8,15};
+    private Colors color;
+
+    public Cinema(Colors color){
+        this.color = color;
+    }
+
+    public Colors getColor() {
+        return color;
+    }
 }
