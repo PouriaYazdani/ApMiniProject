43dce889f64bddf0f7d59c0421ba178b0d71d1e2
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-23 19:45:24 +0430

    implemented EmptyField constructor and added atField array

--- game/properties/EmptyField.java
+++ game/properties/EmptyField.java
@@ -1,4 +1,26 @@
 package game.properties;
 
 public class EmptyField extends BuyableProperties{
+    public final static int[] atFields = {2,7,9,12,14,18,19,23};
+
+    public EmptyField(int atField){
+        switch(atField){
+            case 2:
+            case 12:
+                this.color = Colors.GREEN;
+                break;
+            case 7:
+            case 19:
+                this.color = Colors.YELLOW;
+                break;
+            case 9:
+            case 18:
+                this.color = Colors.RED;
+                break;
+            case 14:
+            case 23:
+                this.color = Colors.BLUE;
+        }
+        this.atField = atField;
+    }
 }
