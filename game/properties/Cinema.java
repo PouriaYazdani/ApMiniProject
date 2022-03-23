984bee711ecdfb8a0625f73cba00364d81ea4c03
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-23 13:31:43 +0430

    updated Cinema class constructor

--- game/properties/Cinema.java
+++ game/properties/Cinema.java
@@ -1,10 +1,26 @@
 package game.properties;
 
 public class Cinema extends BuyableProperties{
-    public final static int[] atFields = {4,8,15};
+    public final static int[] atFields = {4,8,15,22};
 
-    public Cinema(Colors color){
-        this.color = color;
+    public Cinema(int atField){
+        switch (atField){
+            case 4:
+                this.atField = atField;
+                color = Colors.RED;
+                break;
+            case 8:
+                this.atField = atField;
+                color = Colors.BLUE;
+                break;
+            case 15:
+                this.atField = atField;
+                color = Colors.GREEN;
+                break;
+            case 22:
+                this.atField = atField;
+                color = Colors.YELLOW;
+        }
     }
 
     public Colors getColor() {
