48a8dd43f7f749a72829de9a1df61947acd59047
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-23 11:45:52 +0430

    made Board class singelton

--- game/Board.java
+++ game/Board.java
@@ -1,4 +1,13 @@
 package game;
 
 public class Board {
+    private static Board board;
+
+    private Board(){}
+
+    public static Board getBoard() {
+        if(board == null)
+            board = new Board();
+        return board;
+    }
 }
