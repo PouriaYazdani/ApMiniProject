66e0ade65b337ba30d8c3b99afd1f4f1b369d2f9
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-23 20:44:20 +0430

    fixed bug in initialzing feilds array elements in Board constructor

--- game/Board.java
+++ game/Board.java
@@ -11,43 +11,35 @@ public class Board {
         fields = new Field[24];
         for (int i = 0; i < NUM_OF_FIELDS; i++) {
             if(isIn(i, Airport.atFields)){
-                fields[i] = new Airport(i);
+                fields[i] = new Airport(i+1);
             }
             else if(isIn(i, Bank.atFields)){
-                fields[i] = new Bank(i);
+                fields[i] = new Bank(i+1);
             }
             else if(isIn(i, FreeParking.atFields)){
-                fields[i] = new FreeParking(i);
+                fields[i] = new FreeParking(i+1);
             }
             else if(isIn(i, Prison.atFields)){
-                fields[i] = new Prison(i);
+                fields[i] = new Prison(i+1);
             }
             else if(isIn(i,Prize.atFields)){
-                fields[i] = new Prize(i);
+                fields[i] = new Prize(i+1);
             }
             else if(isIn(i,QuestionMark.atFields)){
-                fields[i] = new QuestionMark(i);
+                fields[i] = new QuestionMark(i+1);
             }
             else if(isIn(i,Road.atFields)){
-                fields[i] = new Road(i);
+                fields[i] = new Road(i+1);
             }
             else if(isIn(i,Tax.atFields)){
-                fields[i] = new Tax(i);
+                fields[i] = new Tax(i+1);
             }
             else if(isIn(i,Cinema.atFields)){
-                fields[i] = new Cinema(i);
+                fields[i] = new Cinema(i+1);
             }
             else {//emptyField
-                fields[i] = new EmptyField(i);
+                fields[i] = new EmptyField(i+1);
             }
-
-
-
-
-
-
-
-
         }
     }
 
@@ -65,6 +57,4 @@ public class Board {
         }
         return false;
     }
-
-
 }
