8a00d07fd7e8ea3f140fb3c89d3bc36784379477
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-24 13:26:03 +0430

    updated the structure of BuyableField class

--- game/properties/BuyableProperties.java
+++ game/properties/BuyableProperties.java
@@ -1,7 +1,28 @@
 package game.properties;
 
-public class BuyableProperties extends Field{
+import game.Owner;
+
+public abstract class BuyableProperties extends Field{
     protected Colors color;
+    protected int purchasePrice;
+    protected int currentRentPrice;
+    protected Owner owner;
+
+    public abstract void buy();
+
+    public abstract void sell();
+
+    public int getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public int getCurrentRentPrice() {
+        return currentRentPrice;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
 
     public Colors getColor() {
         return color;
