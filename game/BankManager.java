45fa15f3c9f5dd23ee8ad122c1a8a1fffd5b431b
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-25 11:45:44 +0430

    made BankManager singelton

--- game/BankManager.java
+++ game/BankManager.java
@@ -1,4 +1,15 @@
 package game;
 
 public class BankManager implements Owner{
+    private static BankManager bankManager;
+
+    private BankManager(){
+
+    }
+
+    public static BankManager getInstance() {
+        if(bankManager == null)
+            bankManager = new BankManager();
+        return bankManager;
+    }
 }
