59c4450559aa2dfd21cba9d9dd3e0c8547b53141
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-26 17:36:02 +0430

    implemented buy and sell method in EmptyField class

--- game/properties/EmptyField.java
+++ game/properties/EmptyField.java
@@ -1,13 +1,18 @@
 package game.properties;
 
 import game.*;
+import game.exceptions.IllegalPurchase;
+import game.exceptions.IllegalSell;
+import game.exceptions.NotEnoughCashToBuy;
 import game.properties.*;
 
 public class EmptyField extends BuyableProperties{
     public final static int[] atFields = {2,7,9,12,14,18,19,23};
     private int numberOfBuildings;
-    private boolean isThereHotel;
+    private boolean isThereHotel = false;
     private final int MAXIMUM_BUILDINGS = 4;
+    private final int BUILDING_COST = 150;
+    private final int HOTEL_LICENSE_COST = 100;
     {
         purchasePrice = 100;
         currentRentPrice = 50;
@@ -36,12 +41,45 @@ public class EmptyField extends BuyableProperties{
 
     @Override
     public void buy(Player player){
-
+        if(this.owner != BankManager.getInstance()){
+            if(this.owner == player){
+                throw new IllegalPurchase("You have already purchased this property!");
+            }
+            else{
+                throw new IllegalPurchase("This property belongs to " + (Player)owner.getName()+ " you can't purchase it!");
+            }
+        }
+        else if(purchasePrice > player.getCash()){
+            throw new NotEnoughCashToBuy("You don't have enough money to buy this property! You can sell some of your properties in order to do so.");
+        }
+        player.setCash(player.getCash() - purchasePrice);//get the money
+        this.owner = player;//give the Ownership
+        player.getOwnedProperties().add(this);//add the property to the player's list of owned properties
+        player.setPropertyWorth(player.getPropertyWorth() + purchasePrice/2);//update property worth
+        int newNetWorth = player.getCash() + player.getPropertyWorth();
+        player.setNetWorth(newNetWorth);//update netWorth
     }
 
     @Override
     public void sell(Player player){
-
+        if(this.owner == BankManager.getInstance()){
+            throw new IllegalSell("This property belongs to the Bank you can't sell it!");
+        }
+        else if(this.owner != player){
+            throw new IllegalSell("This property belongs to " + (Player)owner.getName() + "you can't sell it!");
+        }
+        int emptyFieldWorth = purchasePrice + (numberOfBuildings * BUILDING_COST);
+        if(isThereHotel){
+            emptyFieldWorth += HOTEL_LICENSE_COST;
+        }
+        player.setCash(player.getCash() + emptyFieldWorth/2);//give the money
+        this.owner = BankManager.getInstance();//take the Ownership
+        player.getOwnedProperties().remove(this);//remove the sold properties from the player's list of owned properties
+        player.setPropertyWorth(player.getPropertyWorth() - emptyFieldWorth/2);//update property worth
+        int newNetWorth = player.getCash() + player.getPropertyWorth();
+        player.setNetWorth(newNetWorth);//update net worth
+        numberOfBuildings = 0;
+        isThereHotel = false;
     }
 
     @Override
