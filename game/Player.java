638b8e9509a9287d576edda7ef4768fe7527322f
Mammad-Smoke <mohammadbahrami82@outlook.com>
2022-03-26 11:21:19 +0430

    Player property fields added

--- game/Player.java
+++ game/Player.java
@@ -1,3 +1,19 @@
 package game;
+
+import game.properties.BuyableProperties;
+
 public class Player implements Owner {
+    private final String name;
+    private int cash;
+    private int position;
+    private Integer lastDiceNumber;
+    BuyableProperties[] properties;
+    private int propertyWorth;
+    private int netWorth;
+    private int noTax;
+    private int noJail;
+    private int ownedCinemas;
+    private int investedMoney = 0;
+    private int rank;
+    private boolean inJail;
 }
