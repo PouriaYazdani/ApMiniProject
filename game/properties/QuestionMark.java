254e9248c6a71c9b0909a3792cf2106d2fbda454
Mammad-Smoke <mohammadbahrami82@outlook.com>
2022-03-28 23:51:10 +0430

    QuestionMark properties added

--- game/properties/QuestionMark.java
+++ game/properties/QuestionMark.java
@@ -1,9 +1,14 @@
 package game.properties;
 
+import java.util.Random;
+
 public class QuestionMark extends Field{
     public final static int[] atFields = {24};
 
     public QuestionMark(int atField) {
         this.atField = atField;
     }
+    Random random = new Random();
+    int randNumber = random.nextInt(7);
+    LuckyCards[] cards = LuckyCards.values();
 }
