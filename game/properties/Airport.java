bf6819bcb7e279d669b49a503d4979d422c70b0e
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-28 23:06:45 +0430

    added javadoc to Airport class

--- game/properties/Airport.java
+++ game/properties/Airport.java
@@ -4,7 +4,14 @@ import game.Player;
 import game.exceptions.IllegalDestination;
 import game.exceptions.NotEnoughCashToFly;
 
+/**
+ * this class implements airport fields in the board and contains the only command specifically related to it  {@link
+ * #fly(Player, int)}.
+ */
 public class Airport extends BankProperties{
+    /**
+     * is used in initialization process in Board constructor.
+     */
     public final static int[] atFields = {3,11,20};
     private final int FLIGHT_COST = 50;
 
@@ -12,6 +19,12 @@ public class Airport extends BankProperties{
         this.atField = atField;
     }
 
+    /**
+     * throws exceptions which will get caught in gamerunner if the player sends invalid arguments and also charges the
+     * flight cost from the player.
+     * @param player
+     * @param destination
+     */
     public void fly(Player player,int destination){
         if(destination == atField){
             throw new IllegalDestination("You are already in airport NO." + atField + "!");
@@ -25,6 +38,12 @@ public class Airport extends BankProperties{
         player.setCash(player.getCash() - FLIGHT_COST);
         player.setPosition(destination);
     }
+
+    /**
+     * simple method to check if argument x is in {@link #atFields}.
+     * @param x
+     * @return whether if there is an airport in destination or not.
+     */
     private boolean isIn(int x){
         x++;
         for (int i = 0; i < atFields.length; i++) {
