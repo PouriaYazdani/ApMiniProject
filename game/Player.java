ca492b7900ff97f7d2cf44f32f2e893c3263520f
Mammad-Smoke <mohammadbahrami82@outlook.com>
2022-03-28 19:16:30 +0430

    make ownedProperties ArrayList and addd setter & getter

--- game/Player.java
+++ game/Player.java
@@ -9,7 +9,7 @@ public class Player implements Owner {
     private int cash;
     private int position;
     private Integer lastDiceNumber;
-    private BuyableProperties[] ownedProperties;
+    private ArrayList<BuyableProperties> ownedProperties;
     private int propertyWorth;
     private int netWorth;
     private int noTax;
@@ -92,14 +92,14 @@ public class Player implements Owner {
         return inJail;
     }
 
-    public BuyableProperties[] getOwnedProperties() {
+    public ArrayList<BuyableProperties> getOwnedProperties() {
         return ownedProperties;
     }
 
-    public void setOwnedProperties(BuyableProperties[] ownedProperties) {
+    public void setOwnedProperties(ArrayList<BuyableProperties> ownedProperties) {
         this.ownedProperties = ownedProperties;
     }
-
+    
     public int getBuiltBuilding() {
         return builtBuilding;
     }
