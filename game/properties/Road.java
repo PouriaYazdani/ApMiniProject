5352142408b4a29e2376d3f255785a4941ce05ac
Mammad-Smoke <mohammadbahrami82@outlook.com>
2022-03-29 21:31:09 +0430

    exception throwing implemented & make a variable for toll

--- game/properties/Road.java
+++ game/properties/Road.java
@@ -1,17 +1,21 @@
 package game.properties;
 
 import game.Player;
+import game.exceptions.NotEnoughCashToPayToll;
 
 /**
  *  every player must pay a 100$ toll then they can pass
  */
 public class Road extends BankProperties{
     public final static int[] atFields = {5,10,16};
-
+    private final double tollPrice = 100.0;
     public Road(int atField) {
         this.atField = atField;
     }
     public void payToll(Player player){
-        player.setCash(player.getCash() - 100);//100$ toll
+        if (player.getCash() < tollPrice){
+            throw new NotEnoughCashToPayToll("you don't have enough cash to pay your toll, try selling your property in order to do so");
+        }
+        player.setCash(player.getCash() - tollPrice);// getting a 100$ toll
     }
 }
