b44d88bce293f10fade99bb12bbdcb8e4e581459
Mammad-Smoke <mohammadbahrami82@outlook.com>
2022-03-30 00:13:57 +0430

    javadoc for class and methods added

--- game/properties/Prison.java
+++ game/properties/Prison.java
@@ -3,6 +3,9 @@ package game.properties;
 import game.Player;
 import game.exceptions.NotEnoughCashForFreedom;
 
+/**
+ *  with this class's methods we'll be able to put a player in jail or get them out
+ */
 public class Prison extends BankProperties{
     public final static int[] atFields = {13};
     private final double FREEDOM_COST = 50.0;
@@ -11,6 +14,9 @@ public class Prison extends BankProperties{
         this.atField = atField;
     }
     public static void imprisonment(Player player){
+        /**
+         * this method checks if the player has a freedom coupon, if they haven't any we'll imprison them
+         */
         if (player.getNoJail() > 0){
             System.out.println("You had a NO_JAIL coupon! Be careful next time.");
             player.setNoJail(player.getNoJail() - 1);
@@ -20,6 +26,9 @@ public class Prison extends BankProperties{
         }
     }
     public void free(Player player){
+        /**
+         * If the players have $ 50, we will release them
+         */
         if (player.getCash() < FREEDOM_COST && player.getLastDiceNumber() != 1){
             throw new NotEnoughCashForFreedom("you haven't enough cash to get outta here, try sell your properties in order to do so");
         }
@@ -28,6 +37,9 @@ public class Prison extends BankProperties{
         System.out.println("you're free to go!");
     }
     public void luckyDice(Player player){
+        /**
+         * If the dice number is 1, they will be released, otherwise they have to pay $ 10 and wait for the next round
+         */
         if (player.getLastDiceNumber() == 1){
             player.setInJail(false);
             System.out.println("you're free to go!");
