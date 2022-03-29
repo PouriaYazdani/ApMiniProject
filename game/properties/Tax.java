a1571119607f3a19a8aa99cb97e8a4165375f581
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-29 18:46:41 +0430

    implemented Tax class

--- game/properties/Tax.java
+++ game/properties/Tax.java
@@ -1,9 +1,21 @@
 package game.properties;
 
+import game.Player;
+
 public class Tax extends BankProperties{
     public final static int[] atFields = {17};
+    private final double TAX_RATIO = 0.1;
 
     public Tax(int atField) {
         this.atField = atField;
+    }
+
+    public void chargeTax(Player player){
+        if(player.getNoTax() > 0){
+            System.out.println("You had a NO_TAX coupon! Bee careful next time.");
+            player.setNoTax(player.getNoTax() - 1);
+        }
+        double chargedTax = player.getCash() * TAX_RATIO;
+        player.setCash(player.getCash() - chargedTax);
     }
 }
