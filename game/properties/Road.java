f719cd47d06532bd3c7b49d5da88ad5e00703243
Mammad-Smoke <mohammadbahrami82@outlook.com>
2022-03-29 20:51:25 +0430

    payToll method added and implemented

--- game/properties/Road.java
+++ game/properties/Road.java
@@ -1,9 +1,14 @@
 package game.properties;
 
+import game.Player;
+
 public class Road extends BankProperties{
     public final static int[] atFields = {5,10,16};
 
     public Road(int atField) {
         this.atField = atField;
+    }
+    public void payToll(Player player){
+        player.setCash(player.getCash() - 100);//100$ toll
     }
 }
