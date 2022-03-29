3257d8abbd4e67f48f02a35908bdde9758b28286
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-29 11:42:53 +0430

    implemented Prize class

--- game/properties/Prize.java
+++ game/properties/Prize.java
@@ -1,9 +1,16 @@
 package game.properties;
 
+import game.Player;
+
 public class Prize extends BankProperties{
     public final static int[] atFields = {6};
+    private final int PRIZE_VALUE = 200;
 
     public Prize(int atField) {
         this.atField = atField;
+    }
+
+    public void wonPrize(Player player){
+        player.setCash(player.getCash() + PRIZE_VALUE);
     }
 }
