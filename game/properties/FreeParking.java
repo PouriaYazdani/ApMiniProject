b16a4ec98ef8b5c7dd117caa2d82255bcbfb0045
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-29 21:38:41 +0430

    implemented freeParking class

--- game/properties/FreeParking.java
+++ game/properties/FreeParking.java
@@ -1,9 +1,14 @@
 package game.properties;
 
+
 public class FreeParking extends Field{
     public final static int[] atFields = {1};
 
     public FreeParking(int atField) {
         this.atField = atField;
+    }
+
+    public void enterParking(){
+        System.out.println("You have entered free parking! enjoy you're free stay for now.");
     }
 }
