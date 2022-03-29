83153b8eeb83214573cf66d1f57c97ab8c1a15b1
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-29 13:10:01 +0430

    implemented structure of Bank class

--- game/properties/Bank.java
+++ game/properties/Bank.java
@@ -1,10 +1,23 @@
 package game.properties;
 
+import game.Player;
+
 public class Bank extends BankProperties{
     public final static int[] atFields = {21};
+    private double INVEST_RATIO = 0.5;
+    private int PROFIT_RATIO = 2;
 
     public Bank(int atField){
         this.atField = atField;
     }
+
+    public void invest(Player player){
+
+    }
+
+    public void profit(Player player){
+
+    }
+
 
 }
