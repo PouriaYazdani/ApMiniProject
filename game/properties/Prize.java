03ce64d8c54031ecf8c2bcad61ad52f43c84f313
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-29 11:48:15 +0430

    added javadoc to Prize class

--- game/properties/Prize.java
+++ game/properties/Prize.java
@@ -2,7 +2,14 @@ package game.properties;
 
 import game.Player;
 
+/**
+ * implements the only prize field in the game, and it's only behaviour {@link #wonPrize(Player)}.
+ */
 public class Prize extends BankProperties{
+    /**
+     * used in initialization process in Board constructor.
+     * @see game.Board
+     */
     public final static int[] atFields = {6};
     private final int PRIZE_VALUE = 200;
 
@@ -10,6 +17,10 @@ public class Prize extends BankProperties{
         this.atField = atField;
     }
 
+    /**
+     * just adds the prize to player's cash budget.
+     * @param player
+     */
     public void wonPrize(Player player){
         player.setCash(player.getCash() + PRIZE_VALUE);
     }
