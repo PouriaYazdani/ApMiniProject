efed595b0fcc7f405c49f9970f2373981b354abd
Mammad-Smoke <mohammadbahrami82@outlook.com>
2022-03-29 15:50:04 +0430

    randomCard body updated

--- game/properties/QuestionMark.java
+++ game/properties/QuestionMark.java
@@ -1,6 +1,7 @@
 package game.properties;
 
 import java.util.Random;
+import game.Player;
 
 public class QuestionMark extends Field{
     public final static int[] atFields = {24};
@@ -8,9 +9,19 @@ public class QuestionMark extends Field{
     public QuestionMark(int atField) {
         this.atField = atField;
     }
-    public static void randomCard(){
-        Random random = new Random();
-        int randNumber = random.nextInt(7);
-        LuckyCards[] cards = LuckyCards.values();
+        public static void randomCard(Player  player){
+            Random random = new Random();
+            int randNumber = random.nextInt(7);
+            LuckyCards[] cards = LuckyCards.values();
+            switch (cards[randNumber]){
+                case CASH -> player.setCash(player.getCash()+200);break;
+                case GOJAIL -> Prison.Imprisonment(player);break;//this method will be at Prison class
+                case TAX -> player.setCash((int) (0.9* player.getCash()));break;
+                case MOVE -> player.setPosition(player.getPosition()+3);break;
+                case OUTJAIL -> player.setNoJail(player.getNoJail()+1);break;
+                case NOTAX -> player.setNoTax(player.getNoTax()+1);break;
+                case GIFT -> giveGift(player);break;//this method will be here in the class as a public static
+            }
+        }
     }
 }
