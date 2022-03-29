eb76a79168327c347f4e556466ade9401b6a0fc5
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-29 13:30:03 +0430

    added javadoc to Bank class

--- game/properties/Bank.java
+++ game/properties/Bank.java
@@ -2,7 +2,14 @@ package game.properties;
 
 import game.Player;
 
+/**
+ * implements the only Bank field and the actions it's capable of via two void methods.
+ */
 public class Bank extends BankProperties{
+    /**
+     * is used in initialization process in Board constructor.
+     * @see game.Board
+     */
     public final static int[] atFields = {21};
     private double INVEST_RATIO = 0.5;
     private int PROFIT_RATIO = 2;
@@ -11,6 +18,10 @@ public class Bank extends BankProperties{
         this.atField = atField;
     }
 
+    /**
+     * Implements the invest command and updates player's budget.
+     * @param player
+     */
     public void invest(Player player){
         double investedMoney = player.getCash() * INVEST_RATIO;
         player.setInvestedMoney((int)investedMoney);
@@ -18,6 +29,11 @@ public class Bank extends BankProperties{
         player.setNetWorth(player.getPropertyWorth() + player.getCash());//update net worth
     }
 
+    /**
+     * is called automatically each time the player enters Bank field and returns the profit if the player's invested
+     * money is not zero,and updates player's budget.
+     * @param player
+     */
     public void profit(Player player){
         if(player.getInvestedMoney() != 0){
             int addedCash = player.getInvestedMoney() * PROFIT_RATIO;
