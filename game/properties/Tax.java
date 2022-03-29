5217963c77869303c650747a59dce28220ab152a
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-29 20:17:47 +0430

    added javadoc to Tax class

--- game/properties/Tax.java
+++ game/properties/Tax.java
@@ -2,7 +2,14 @@ package game.properties;
 
 import game.Player;
 
+/**
+ * implements the only tax field in the game.
+ */
 public class Tax extends BankProperties{
+    /**
+     * used in initialization process in Board constructor.
+     * @see game.Board
+     */
     public final static int[] atFields = {17};
     private final double TAX_RATIO = 0.1;
 
@@ -10,6 +17,11 @@ public class Tax extends BankProperties{
         this.atField = atField;
     }
 
+    /**
+     * is called automatically each time a player enters tax field and will make the player pay the tax with specific
+     * {@link #TAX_RATIO}.
+     * @param player
+     */
     public void chargeTax(Player player){
         if(player.getNoTax() > 0){
             System.out.println("You had a NO_TAX coupon! Bee careful next time.");
