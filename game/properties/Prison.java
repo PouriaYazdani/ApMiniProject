624c7b2a3a3810d2bf12d6dcf02b47314efbf341
Mammad-Smoke <mohammadbahrami82@outlook.com>
2022-03-29 22:48:15 +0430

    imprisonment method added

--- game/properties/Prison.java
+++ game/properties/Prison.java
@@ -1,10 +1,21 @@
 package game.properties;
 
+import game.Player;
+
 public class Prison extends BankProperties{
     public final static int[] atFields = {13};
 
     public Prison(int atField){
         this.atField = atField;
+    }
+    public static void imprisonment(Player player){
+        if (player.getNoJail() > 0){
+            System.out.println("You had a NO_JAIL coupon! Be careful next time.");
+            player.setNoJail(player.getNoJail() - 1);
+        } else {
+            player.setInJail(true);
+            player.setPosition(atFields[0]); // change the position of player to it's
+        }
     }
 
 }
