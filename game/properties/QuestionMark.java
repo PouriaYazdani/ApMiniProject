b1033143361ff171cd64d6d9b0fa4f620ea1f63c
Mammad-Smoke <mohammadbahrami82@outlook.com>
2022-03-30 13:36:09 +0430

    switch-case syntax error fixed

--- game/properties/QuestionMark.java
+++ game/properties/QuestionMark.java
@@ -21,13 +21,13 @@ public class QuestionMark extends Field{
             int randNumber = random.nextInt(7);
             LuckyCards[] cards = LuckyCards.values();
             switch (cards[randNumber]){
-                case CASH -> player.setCash(player.getCash()+200);break;
-                case GOJAIL -> Prison.imprisonment(player);break;//this method will be at Prison class
-                case TAX -> player.setCash(0.9* player.getCash());break;
-                case MOVE -> player.setPosition(player.getPosition()+3);break;
-                case OUTJAIL -> player.setNoJail(player.getNoJail()+1);break;
-                case NOTAX -> player.setNoTax(player.getNoTax()+1);break;
-                case GIFT -> giveGift(player);break;//this method will be here in the class as a public static
+                case CASH : player.setCash(player.getCash()+200);break;
+                case GOJAIL : Prison.imprisonment(player);break;//this method will be at Prison class
+                case TAX : player.setCash(0.9* player.getCash());break;
+                case MOVE : player.setPosition(player.getPosition()+3);break;
+                case OUTJAIL : player.setNoJail(player.getNoJail()+1);break;
+                case NOTAX : player.setNoTax(player.getNoTax()+1);break;
+                case GIFT : giveGift(player);break;//this method will be here in the class as a public static
             }
         }
     public  void giveGift(Player p){
