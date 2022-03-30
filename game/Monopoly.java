e88350c9d2a9c94ac34a24957495219fb234076c
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-30 18:25:14 +0430

    implemented waitingMenu method in Monopoly class

--- game/Monopoly.java
+++ game/Monopoly.java
@@ -1,6 +1,10 @@
 package game;
 
+import game.exceptions.IllegalCommand;
+import game.properties.Colors;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.Timer;
 
 public class Monopoly {
@@ -9,9 +13,26 @@ public class Monopoly {
     private static ArrayList <Player> players;
     private Timer timer;
     private int gameDuration;//in minutes
+    private String stringCommand;
+    private Commands enumCommand;
 
     public void waitingMenu(){
-
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            try {
+                stringCommand = scanner.next();
+                enumCommand = commandProcessor(stringCommand);
+                if(enumCommand == Commands.CREATE_GAME){
+                    break;
+                }else{
+                    throw new IllegalCommand("You have entered Illegal command ! Please try again.");
+                }
+            }catch (IllegalArgumentException e){
+                System.out.println("You have entered invalid command ! Please try again.");
+            }catch (IllegalCommand e){
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public void gameInit(){
@@ -23,7 +44,7 @@ public class Monopoly {
     }
 
     private Commands commandProcessor(String command){
-        
+        return  Commands.valueOf(command.toUpperCase());
     }
 
     public static ArrayList<Player> getPlayers() {
