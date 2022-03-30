50e0ccbf0bca99e07c160b02469f7e4fb738cf88
PouriaYazdani <pouria.yazdanii82@yahoo.com>
2022-03-30 12:19:29 +0430

    implemented structure of Monopoly class

--- game/Monopoly.java
+++ game/Monopoly.java
@@ -1,4 +1,29 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.Timer;
+
 public class Monopoly {
+    private Board board;
+    private BankManager bankManager;
+    public static ArrayList players;
+    private Timer timer;
+    private int gameDuration;//in minutes
+
+    public void waitingMenu(){
+
+    }
+
+    public void gameInit(){
+
+    }
+
+    public void gamerunner(){
+
+    }
+
+    private Commands commandProcessor(String command){
+        
+    }
+
 }
